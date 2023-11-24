394e07d45cd53d3be915362a489c6bc71ab7a40b
JulyMaker <julio.martin.saez@gmail.com>
2023-11-24 10:57:21 +0100

    paths ignore

--- 2023/utils/paths.h
+++
@@ -1,2 +0,0 @@
-#define DAY_PATH(day) "E:/personal/Repos/AdventOfCode/2023/inputs/input"+day+".txt"
-#define DAY_EXAMPLE_PATH(day) "E:/personal/Repos/AdventOfCode/2023/examples/inputExample"+day+".txt"
