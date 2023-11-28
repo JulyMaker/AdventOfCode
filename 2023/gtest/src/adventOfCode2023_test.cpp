2e6305b084bee79218e6b213d774f66354e77f49
JulyMaker <julio.martin.saez@gmail.com>
2023-11-28 11:23:10 +0100

    2023 AOC Code init

--- 2023/gtest/src/adventOfCode2023_test.cpp
+++ 2023/gtest/src/adventOfCode2023_test.cpp
@@ -115,6 +115,182 @@ TEST(AdventOfCode2023, advOcodeD_09)
     EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
 }
 
+TEST(AdventOfCode2023, advOcodeD_10)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_11)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_12)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_13)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_14)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_15)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_16)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_17)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_18)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_19)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_20)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_21)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_22)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_23)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_24)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
+TEST(AdventOfCode2023, advOcodeD_25)
+{
+    const std::string testName = testing::UnitTest::GetInstance()->current_test_info()->name();
+    std::string FUNCTIONNAME1; std::string FUNCTIONNAME2;
+
+    std::ifstream& inputFile = getData(testName, FUNCTIONNAME1, FUNCTIONNAME2);
+
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME1](inputFile));
+    EXPECT_EQ(0, launchProblem[FUNCTIONNAME2](inputFile));
+}
+
 int main(int argc, char **argv) 
 {
   testing::InitGoogleTest(&argc, argv);
