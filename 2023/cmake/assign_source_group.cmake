af0d5913ee10c5455c0aaecefe94fed7e2f0a9e1
JulyMaker <julio.martin.saez@gmail.com>
2023-11-29 17:51:16 +0100

    2023 AOC Replace .. path in visual studio

--- 2023/cmake/assign_source_group.cmake
+++ 2023/cmake/assign_source_group.cmake
@@ -3,9 +3,12 @@ function(assign_source_group)
       foreach(_file IN ITEMS ${ARGN})
       # Get the directory of the source file
       get_filename_component(_parentDir "${_file}" DIRECTORY)
-
+      
       # Remove common directory prefix to make the group
       string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}" "" _group "${_parentDir}")
+
+      # Make sure we are using my and not ..
+      string(REPLACE ".." "_myJuly" _group "${_group}")
 
       # Make sure we are using windows slashes
       string(REPLACE "/" "\\" _group "${_group}")
