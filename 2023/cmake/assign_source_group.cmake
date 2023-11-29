0622a173daed99936cfffaf4114363136b417b7f
JulyMaker <julio.martin.saez@gmail.com>
2023-11-29 17:39:39 +0100

    2023 AOC cmake functions folder

---
+++ 2023/cmake/assign_source_group.cmake
@@ -0,0 +1,15 @@
+#Keep directories structure for Visual Studio
+function(assign_source_group)
+      foreach(_file IN ITEMS ${ARGN})
+      # Get the directory of the source file
+      get_filename_component(_parentDir "${_file}" DIRECTORY)
+
+      # Remove common directory prefix to make the group
+      string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}" "" _group "${_parentDir}")
+
+      # Make sure we are using windows slashes
+      string(REPLACE "/" "\\" _group "${_group}")
+
+      source_group("${_group}" FILES "${_file}")
+  endforeach()
+endfunction(assign_source_group)
