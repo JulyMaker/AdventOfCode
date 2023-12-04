71cab7dbf39039240ee05d639b40e9521f0b8bf0
JulyMaker <julio.martin.saez@gmail.com>
2023-12-04 18:27:33 +0100

    2023 Day 04 - Scratchcards (version 2.0 with sets)

--- 2023/code/src/day04_Scratchcards.cpp
+++ 2023/code/src/day04_Scratchcards.cpp
@@ -1,34 +1,29 @@
 #include <days.h>
 
+void getCard(const std::vector<std::string>& scratchcards, const int id, std::vector<int>& win, std::vector<int>& numbers)
+{
+    win = splitI(scratchcards[id * 3 + 1], " ");
+    numbers = splitI(scratchcards[id * 3 + 2], " ");
+    std::sort(win.begin(), win.end());
+    std::sort(numbers.begin(), numbers.end());
+}
+
 uint64_t adventDay04P12023(std::ifstream& input)
 {
     uint64_t score = 0;
     std::string result;
 
+    std::vector<int> win, numbers;
     std::vector<std::string> cards = parseInputReg(input, "Card .*: (.*) \\| (.*)");
-    for (int i= 0; i < cards.size()-2; i+= 3)
+    for (int cardId= 0; cardId < cards.size()/3; cardId++)
     {
-        std::vector<int> win = splitI(cards[i + 1]," ");
-        std::sort(win.begin(), win.end());
-        std::vector<int> numbers = splitI(cards[i + 2], " ");
-        std::sort(numbers.begin(), numbers.end());
+        getCard(cards, cardId, win, numbers);
 
-        int points = 0;
-        int winPointer = 0;
-        for (auto& num : numbers)
-        {
-            while ( (winPointer < win.size()) && num > win[winPointer])
-                winPointer++;
+        std::vector<int> intersection;
+        std::set_intersection(win.begin(), win.end(), numbers.begin(), numbers.end(),
+            std::back_inserter(intersection));
 
-            if (winPointer > win.size() - 1) break;
-
-            if (num == win[winPointer])
-            {
-                points = (points == 0) ? 1 : points * 2;
-                winPointer++;
-            } 
-        }
-        score+= points;
+        score+= (intersection.size() == 0)? 0 : std::pow(2, intersection.size()-1);
     }
 
     result = std::to_string(score);
@@ -36,70 +31,34 @@ uint64_t adventDay04P12023(std::ifstream& input)
     return score;
 }
 
-using Card  = std::tuple<int, std::vector<int>, std::vector<int>>; // Copies, winNumbers, gameNumbers
-using Cards = std::vector<Card>;
-
-void getCard(const std::vector<std::string>& scratchcards, const int id, std::vector<int>& win, std::vector<int>& numbers)
-{
-    win = splitI(scratchcards[id + 1], " ");
-    std::sort(win.begin(), win.end());
-    numbers = splitI(scratchcards[id + 2], " ");
-    std::sort(numbers.begin(), numbers.end());
-}
 
 uint64_t adventDay04P22023(std::ifstream& input)
 {
     uint64_t score = 0;
     std::string result;
 
-    Cards cards;
-    int cardId = 0;
     std::vector<int> win, numbers;
-
-    std::vector<std::string> scratchcards = parseInputReg(input, "Card\\s+\\d+: (.*) \\| (.*)");
-
-    for(int i=0; i < scratchcards.size() - 2; i+= 3)
-    {
-        getCard(scratchcards, i, win, numbers);
-        Card c = { 1, win, numbers };
-        cards.push_back(c);
-    }
-
+    std::vector<std::string> scratchcards = parseInputReg(input, "Card .*: (.*) \\| (.*)");
+    std::vector<int> cardsCopies = std::vector<int>(scratchcards.size()/3, 1);
     
-    for(int cardId= 0; cardId < cards.size(); cardId++)
+    for(int cardId= 0; cardId < cardsCopies.size(); cardId++)
     {
-        win = std::get<1>(cards[cardId]);
-        numbers = std::get<2>(cards[cardId]);
+        getCard(scratchcards, cardId, win, numbers);
 
-       int winTimes = 0;
-       int winPointer = 0;
-       for (auto& num : numbers)
+        std::vector<int> intersection;
+        std::set_intersection(win.begin(), win.end(), numbers.begin(), numbers.end(),
+            std::back_inserter(intersection));
+
+
+       for (int times = 1; times <= intersection.size(); times++)
        {
-           while ((winPointer < win.size()) && num > win[winPointer])
-               winPointer++;
-       
-           if (winPointer > win.size() - 1) break;
-       
-           if (num == win[winPointer])
-           {
-               winTimes++;
-               winPointer++;
-           }
+           cardsCopies[cardId + times]+= cardsCopies[cardId];
        }
 
-       for (int times = 1; times <= winTimes; times++)
-       {
-          std::get<0>(cards[cardId + times])+= std::get<0>(cards[cardId]);
-       }
-
-       score+= std::get<0>(cards[cardId]);
+       score+= cardsCopies[cardId];
     }
 
     result = std::to_string(score);
     std::cout << "El resultado es: " << result << std::endl;
     return score;
 }
-
-//std::get<0>(tpl) = 1;
-//std::get<1>(tpl) = 2;
-//std::get<2>(tpl) = 3;
